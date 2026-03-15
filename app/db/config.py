045446bad808da74407d8bb0705f394c1014ebf0
Yeung Ka Chun <siublackbusiness@gmail.com>
2026-03-15 22:40:15 +0800

    feat : remove config

--- app/db/config.py
+++ app/db/config.py
@@ -1,6 +1,6 @@
 DB_CONFIG = {
     "host": "localhost",
     "user": "",  #Enter your local host username 
-    "password": "975314", #Enter your local host password
+    "password": "", #Enter your local host password
     "database": "universityrecordsystem"
 }
